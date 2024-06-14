721794bff57fcada336c8f27fb6f998d7b2e6b77
Raquel Ramos <rdsr1304@gmail.com>
2024-06-14 10:38:40 -0300

    Initial Commit

---
+++ 3dcubetest/_dcubetestApp.swift
@@ -0,0 +1,17 @@
+//
+//  _dcubetestApp.swift
+//  3dcubetest
+//
+//  Created by Raquel Ramos on 14/06/24.
+//
+
+import SwiftUI
+
+@main
+struct _dcubetestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
